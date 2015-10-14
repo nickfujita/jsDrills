1960659e385b7c05aad7a770b59fe7012a8485b8
nicolasfujita <nickfujita@gmail.com>
2015-10-13 22:06:24 -0700

    added testing html for backtracking js files, completed implementation of string permutations

--- backtracking/stringPermutations.js
+++ backtracking/stringPermutations.js
@@ -1,8 +1,33 @@
 //find all possible combinations of a given string
 //eg. 'cat' -> ['cat','cta','act','atc','tac','tca']
 
-var generateStringPermutations = function (inputString) {
+var stringPermutations = function (inputString) {
 
+  var results = [];
+
+  var generatePermutations = function(currentPernutation) {
+    if(!currentPernutation) { currentPernutation = ''; }
+
+    if(currentPernutation.length === inputString.length){
+      results.push(currentPernutation);
+      return;
+    }
+
+    for(var i in inputString) {
+      if(currentPernutation.indexOf(inputString[i]) < 0) {
+        currentPernutation += inputString[i];
+        generatePermutations(currentPernutation);
+        currentPernutation = currentPernutation.slice(0,currentPernutation.length-1);
+      }
+    }
+
+  };
+
+  generatePermutations();
+
+  return results;
+
+};
   //create temp array to hold all combinations of the string
 
   //inner function that can recurse over the different combinations of the word
@@ -20,4 +45,3 @@ var generateStringPermutations = function (inputString) {
   //return results
 
 
-};
