9c224a20bf766340d30658230088ed9003facbc3
nicolasfujita <nickfujita@gmail.com>
2015-10-14 00:24:49 -0700

    implemented second version of string permutations that will accept duplicate characters in the input. the original one would only return unique characters

--- backtracking/stringPermutations.js
+++ backtracking/stringPermutations.js
@@ -44,4 +44,59 @@ var stringPermutations = function (inputString) {
 
   //return results
 
+//create a results variable to store the results
 
+//inner function that will recurse to get all permutations (currentPermutation, currentIndex)
+  //initialize currentPermutation and currentIndex if not already provided
+  
+  //check to see if the current index equals the length of the string
+    //this means that we have generated a string of the same length
+    //push the string into the results array
+      //extract the values from their tupels
+    
+  //loop through each of the possible strings to be inserted to the current string
+    //if the tupel of the current position is not already in current array
+      //push the tupel into the current array [i,val]
+      //recurse
+      //pop the tupel back off the current array
+  
+
+//initial call to the inner function
+
+//return results
+
+function permutations(string) {
+
+  var results = [];
+  
+  var generatePermutations = function(currentPermutation, currentIndex) {
+    currentPermutation = currentPermutation || [];
+    currentIndex = currentIndex || 0;
+    
+    if(currentIndex === string.length) {
+      // debugger;
+      var permutation = currentPermutation.reduce(function(accum,tupel) {
+        return accum.concat(JSON.parse(tupel)[1]);
+      },'');
+      
+      if(results.indexOf(permutation) < 0) {
+        results.push(permutation);
+      }
+      return;
+    }
+    
+    for(var i in string) {
+      if(currentPermutation.indexOf(JSON.stringify( [i,string[i]] )) < 0) {
+        currentPermutation.push(JSON.stringify([i,string[i]]));
+        generatePermutations(currentPermutation,currentIndex + 1);
+        currentPermutation.pop();
+      }
+    }
+  };
+
+  generatePermutations();
+  
+  return results;
+
+
+}
