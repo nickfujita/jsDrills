409722a3bb9d9129c02206dc605661e99ba3aea2
nicolasfujita <nickfujita@gmail.com>
2015-10-13 19:08:01 -0700

    init blank stringPermutations problem

---
+++ backtracking/stringPermutations.js
@@ -0,0 +1,9 @@
+//find all possible combinations of a given string
+//eg. 'cat' -> [['cat'],['cta'],['act'],['atc'],['tac'],['tca']]
+
+var generateStringPermutations = function (inputString) {
+
+
+
+
+};
