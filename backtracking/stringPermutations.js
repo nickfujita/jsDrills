7786b9a8474accaccdde09416aadaede9aa21b74
nicolasfujita <nickfujita@gmail.com>
2015-10-13 19:13:55 -0700

    stringPermutations pseudocode

--- backtracking/stringPermutations.js
+++ backtracking/stringPermutations.js
@@ -1,9 +1,23 @@
 //find all possible combinations of a given string
-//eg. 'cat' -> [['cat'],['cta'],['act'],['atc'],['tac'],['tca']]
+//eg. 'cat' -> ['cat','cta','act','atc','tac','tca']
 
 var generateStringPermutations = function (inputString) {
 
+  //create temp array to hold all combinations of the string
 
+  //inner function that can recurse over the different combinations of the word
+    
+    // check to see if the charIndex is the same is the length of the input string
+      //if so, then push it into the array
+
+    //for loop to loop through the letters in the string
+      //add a letter to a string
+      //recurse to next position
+      //remove letter from string
+
+  // inital call to the inner function
+
+  //return results
 
 
 };
